19f6cdb1f1afa26facf931b956c18c24c5f856e1
DucKyWay <niji2549@gmail.com>
2026-01-08 09:35:55 +0700

    test structure

---
+++ lib/constants/styles.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+const Color primaryColor = Color(0xff367812);
+const Color secondaryColor = Color(0xffffffff);
+
+const TextStyle titleText = TextStyle(
+  fontFamily: 'Bai Jamjuree',
+  fontSize: 28,
+  fontWeight: FontWeight.bold,
+  color: Colors.black,
+);
